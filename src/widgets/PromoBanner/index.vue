e8b00481f65def4fcddc8eb1e43635de1acd331e
Muhammadsakhi Sultonzoda <msultonzoda.spka@gmail.com>
2026-08-19 16:02:33 +0500

    feat: add PromoBanner widget

--- src/widgets/PromoBanner/index.vue
+++ src/widgets/PromoBanner/index.vue
@@ -17,12 +17,12 @@ withDefaults(
   <component :is="isEmbedded ? 'div' : 'section'" :class="!isEmbedded ? 'py-xl md:pb-2xl' : ''">
     <div :class="!isEmbedded ? 'container' : ''">
       <div
-        class="relative rounded-radius-lg overflow-hidden min-h-[300px] md:min-h-[300px] flex items-end bg-[url('https://placehold.co/1200x300/1a1a2e/6b7280?text=Promo+Car')] bg-cover bg-center"
+        class="relative rounded-radius-lg overflow-hidden min-h-[220px] md:min-h-[300px] flex items-end bg-[url('/dushanbe_city_night-fhd.png')] bg-cover bg-center"
       >
         <div class="absolute inset-0 bg-gradient-to-r from-black/65 via-black/20 to-transparent" />
-        <!-- <div class="relative z-[1] p-xl">
+        <div class="relative z-[1] p-xl">
           <DButton theme="primary" size="md"> Подробнее </DButton>
-        </div> -->
+        </div>
       </div>
     </div>
   </component>
